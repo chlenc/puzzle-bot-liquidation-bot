8a86273a181cb785dbeff2e541f1bf8f05a97ea6
lzhabo <lidia.zhabo99@gmail.com>
2021-08-29 21:08:37 +0300

    updated msg

--- src/messages_lib.ts
+++ src/messages_lib.ts
@@ -10,7 +10,8 @@ const msg = {
     "[Waves Ducks](https://wavesducks.com/) is a game centered on collectable digital duck images, developed for active members of the Waves ecosystem. In this game, users acquire and collect digital images of ducks, which we call Waves Ducks\n\n" +
     "To get daily game stats please click here 👉🏻 /stats !",
   loading: "Loading data from the blockchain – may take some time...",
-  correct_wallet_address: "Good! Since now you you will be getting changes!🤌🏻",
+  correct_wallet_address:
+    "Good! Since now you you will be getting changes!🤌🏻/\n\n To cancel subscription send me /cancel",
   wrong_wallet_address: "Ooops... seems like this address is not correct!😮",
   cancel_subsc: "Subscription has been canceled😮",
 };
