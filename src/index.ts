d5e7e1152e86bb817895abfe01f5e125c3edca6f
lzhabo <lidia.zhabo99@gmail.com>
2021-09-07 20:53:03 +0300

    fix reg ex for /start

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import * as commitCount from "git-commit-count";
 import telegramService from "./services/telegramService";
 import {
-    checkWalletAddress,
-    getCurrentWavesRate,
+  checkWalletAddress,
+  getCurrentWavesRate,
 } from "./services/statsService";
 import {
-    findByTelegramIdAndUpdate,
-    getUserById,
-    updateUserActivityInfo,
+  findByTelegramIdAndUpdate,
+  getUserById,
+  updateUserActivityInfo,
 } from "./controllers/userController";
 import { User } from "./models/user";
 import msg from "./messages_lib";
 import { initMongo } from "./services/mongo";
 import {
-    sendStatisticMessageToChannels,
-    watchOnAuction,
-    watchOnDucks,
-    watchOnStats,
+  sendStatisticMessageToChannels,
+  watchOnAuction,
+  watchOnDucks,
+  watchOnStats,
 } from "./services/crons";
 import { getStatisticFromDB } from "./controllers/statsController";
 import { createMessage } from "./controllers/messageController";
@@ -27,78 +27,78 @@ initMongo().then();
 
 const parse_mode = "Markdown";
 
-telegramService.telegram.on("message", async (msg, meta) => {
-    await createMessage(msg.from.id, msg.text);
-    await updateUserActivityInfo(msg.from);
+telegramService.telegram.on("message", async (msg) => {
+  await createMessage(msg.from.id, msg.text);
+  await updateUserActivityInfo(msg.from);
 });
 
 telegramService.telegram.onText(
-    /\/start[ \t](.+)/,
-    async ({ chat, from }, match) => {
-        const user = await getUserById(from.id);
-        user != null &&
-        match[1] &&
-        (await User.findByIdAndUpdate(user._id, {
-            invitationChannel: match[1],
-        }));
-        await telegramService.telegram.sendMessage(chat.id, msg.welcome, {
-            parse_mode,
-        });
-    }
+  /\/start[ \t]*(.*)/,
+  async ({ chat, from }, match) => {
+    const user = await getUserById(from.id);
+    user != null &&
+      match[1] &&
+      (await User.findByIdAndUpdate(user._id, {
+        invitationChannel: match[1],
+      }));
+    await telegramService.telegram.sendMessage(chat.id, msg.welcome, {
+      parse_mode,
+    });
+  }
 );
 
 telegramService.telegram.onText(
-    /\/address[ \t](.+)/,
-    async ({ chat, from }, match) => {
-        const address = match[1];
+  /\/address[ \t](.+)/,
+  async ({ chat, from }, match) => {
+    const address = match[1];
 
-        const isValidAddress = await checkWalletAddress(address).catch(() => false);
-        if (!isValidAddress) {
-            return await telegramService.telegram.sendMessage(
-                chat.id,
-                msg.wrong_wallet_address
-            );
-        }
-        await findByTelegramIdAndUpdate(from.id, {
-            walletAddress: address,
-        });
-        await telegramService.telegram.sendMessage(
-            chat.id,
-            msg.correct_wallet_address
-        );
+    const isValidAddress = await checkWalletAddress(address).catch(() => false);
+    if (!isValidAddress) {
+      return await telegramService.telegram.sendMessage(
+        chat.id,
+        msg.wrong_wallet_address
+      );
     }
+    await findByTelegramIdAndUpdate(from.id, {
+      walletAddress: address,
+    });
+    await telegramService.telegram.sendMessage(
+      chat.id,
+      msg.correct_wallet_address
+    );
+  }
 );
 
 telegramService.telegram.onText(/\/cancel/, async ({ chat, from }) => {
-    await findByTelegramIdAndUpdate(from.id, {
-        walletAddress: null,
-        auctionDucks: null,
-        farmingDucks: null,
-        userDucks: null,
-        bids: null,
-    });
-    await telegramService.telegram.sendMessage(chat.id, msg.cancel_subsc);
+  await findByTelegramIdAndUpdate(from.id, {
+    walletAddress: null,
+    auctionDucks: null,
+    farmingDucks: null,
+    userDucks: null,
+    bids: null,
+  });
+  await telegramService.telegram.sendMessage(chat.id, msg.cancel_subsc);
 });
 
 telegramService.telegram.onText(/\/id/, async ({ chat: { id } }) => {
-    await telegramService.telegram.sendMessage(id, String(id));
+  await telegramService.telegram.sendMessage(id, String(id));
 });
 
 telegramService.telegram.onText(/\/rate/, async ({ chat: { id } }) => {
-    const rate = await getCurrentWavesRate();
-    await telegramService.telegram.sendMessage(id, rate);
+  const rate = await getCurrentWavesRate();
+  await telegramService.telegram.sendMessage(id, rate);
 });
 
 telegramService.telegram.onText(/\/version/, async ({ chat: { id } }) => {
-    await telegramService.telegram.sendMessage(
-        id,
-        commitCount("chlenc/big-black-duck-bot/")
-    );
+  await telegramService.telegram.sendMessage(
+    id,
+    commitCount("chlenc/big-black-duck-bot/")
+  );
 });
 
 telegramService.telegram.onText(/\/stats/, async ({ from, chat: { id } }) => {
-    const stats = await getStatisticFromDB();
-    await telegramService.telegram.sendMessage(id, stats, { parse_mode });
+  const stats = await getStatisticFromDB();
+  await telegramService.telegram.sendMessage(id, stats, { parse_mode });
 });
 
 cron.schedule("* * * * *", watchOnStats);
