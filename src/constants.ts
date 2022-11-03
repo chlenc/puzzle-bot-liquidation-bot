26bc54629552e0cc0d706d9119898ea30f2d1df7
alexey <alexnagornnyy.an@gmail.com>
2022-11-03 16:57:55 +0000

    added pool

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
-export const POOLS = ["3P4uA5etnZi4AmBabKinq2bMiWU8KcnHZdH"];
+export const POOLS = [
+  "3P4uA5etnZi4AmBabKinq2bMiWU8KcnHZdH",
+  "3P4DK5VzDwL3vfc5ahUEhtoe5ByZNyacJ3X",
+];
 export const LIMIT_ORDERS = "3PPrfNMnk8z8QhZcqMyJk69mF65s2Rbz3B6";
 export const AGGREGATOR = "3PGFHzVGT4NTigwCKP1NcwoXkodVZwvBuuU";
 export const USDT_ASSET_ID = "34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ";
